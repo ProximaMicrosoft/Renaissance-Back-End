bc80d28dbb3c34b4a968b1815c3cd2bfacfdcdff
guiinfo3333 <antonioguilhermeinfo@gmail.com>
2021-07-01 21:30:59 -0500

    adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import routes from './routes';
-import { Request, Response, NextFunction } from 'express';
 import path from 'path';
+import cors from 'cors'
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express()
+app.use(cors());
 app.use(express.json())
 app.use(routes)
 app.use(express.static(path.join(__dirname, "..", "public")))
